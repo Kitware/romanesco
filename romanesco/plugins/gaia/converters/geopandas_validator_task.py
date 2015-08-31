21b245c1b8030fff2d9f62d1898351270cd6a3c9
Christopher Kotfila <chris.kotfila@kitware.com>
2015-08-31 14:03:53 -0400

    Removes AST parsing code
    This removes the AST prototype parsing code in favor of plain dicts
    for managing validation and conversion of gdal driver types.

--- romanesco/plugins/gaia/converters/geopandas_validator_task.py
+++
@@ -1,7 +0,0 @@
-from romanesco.plugins.gaia.classes import Geo, Output
-import geopandas
-# Intentionally leave off format from Geo(), this
-# will be added by the gaia import_validators() function
-output = Output(isinstance(Geo(input), geopandas.GeoDataFrame),
-                type="boolean",
-                format="boolean")
